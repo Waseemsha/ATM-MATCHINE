6157cac80ada43bc35c8719c6da41387e9ba3bcc
Waseemsha <159756174+Waseemsha@users.noreply.github.com>
2024-05-07 11:23:39 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,70 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "enter your pin",
+            type: "number"
+        }
+    ]
+);
+//   12345 ===  1234 - false
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code !!!!");
+
+   let operationAns = await inquirer.prompt(
+        [
+             {
+                name: "operation",
+                message: "please select option",
+                type: "list",
+                choices: ["witchdraw" ,"check balance"]
+             }
+            
+        
+        ]
+    );
+    
+    console.log(operationAns);
+
+    if (operationAns.operation === "witchdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number",
+                }
+            ]
+        );
+
+        
+        myBalance -= amountAns.amount;
+
+        console.log ("your remaining balance is : " + myBalance)
+       
+    }   else if (operationAns.operation === "check balance"){
+        console.log("your balance is: " + myBalance)
+    }
+ } 
+ 
+ else {
+    console.log("Incorrect pin number");
+ 
+};
+            
+
+            
+        
+
+
+
+    
+    
